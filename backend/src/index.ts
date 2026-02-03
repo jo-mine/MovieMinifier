362dade6506c4264f391ff1c38d8b63ab16720b1
jo-mine <minetab25@gmail.com>
2026-02-03 12:58:17 +0000

    biome auto format

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono().get("/", (c) => {
+	return c.text("Hello Hono!");
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+export type backendApp = typeof app;
 
 export default {
-  fetch: app.fetch,
-  port: 3100,
-}
+	fetch: app.fetch,
+	port: 3100,
+};
